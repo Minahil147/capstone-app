ca9c3ab3b014b239b9819c43dfcdae249d88efce
Minahil <miahilansari123@gmail.com>
2026-08-03 06:54:55 +0500

    added week 2 pactice questions

--- test.js
+++ test.js
@@ -21,3 +21,16 @@ console.log(getNumberStats([2, 4, 6, 8]));
 console.log(getNumberStats([]));
 
 console.log(getNumberStats([100]));
+
+
+const express = require("express"); 
+const router = express.Router(); 
+const { 
+  getAllIssues, getIssueById, createIssue, updateIssue, deleteIssue, 
+} = require("../controllers/issueController"); 
+router.get("/", getAllIssues); 
+router.get("/:id", getIssueById); 
+router.post("/", createIssue); 
+router.put("/:id", updateIssue); 
+router.delete("/:id", deleteIssue); 
+module.exports = router; 
